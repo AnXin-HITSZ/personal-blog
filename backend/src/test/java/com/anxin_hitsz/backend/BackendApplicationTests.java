dce39d9d97d9afa4f09b6840d2dc7c5975709b96
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-05-12 15:44:03 +0800

    feat(backend): initialize SpringBoot backend project
    - Create backend directory structure

---
+++ backend/src/test/java/com/anxin_hitsz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anxin_hitsz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
